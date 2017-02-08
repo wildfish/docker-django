647bea9fd38f8d8a0563630191902c8cb320cca6
Dan Bate <dan@wildfish.com>
2017-02-08 12:34:30 +0000

    set origin to be accessed by ssh not https for deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 set -e;
-
-chmod 600 deployer;
-eval `ssh-agent -s`;
-ssh-add deployer;
+set -x;
 
 wget https://gist.githubusercontent.com/OmegaDroid/22b6e15b2050841d5733095509a97258/raw/e1c8cf52ee3e139c06fa5ac81e4a326621fdd20e/up_version.py;
 chmod +x up_version.py;
 
+git remote set-url origin "git@github.com:wildfish/django-base.git"
 git config --global user.email "deployer@wildfish.com";
 git config --global user.name "WildfishDeployer";
 
@@ -17,4 +15,4 @@ echo "New tag: ${TAG}"
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}";
 
-./push.sh $TAG;
+../push.sh $TAG;
